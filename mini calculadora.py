3dc550b2d3811d40550db11d98e295513646029a
Felpo <70416991+Felpomano@users.noreply.github.com>
2026-07-25 22:05:32 -0300

    Update mini calculadora.py
    now it is possible to show the square root of the result

--- mini calculadora.py
+++ mini calculadora.py
@@ -1,22 +1,33 @@
+import math
+
 def main():
     while True:
 
         print("Mini calculadora de dois números: ")
 
-        num = int(input("Digite um número: "))
-        num2 = int(input("Digite outro número: "))
+        num = float(input("Digite um número: "))
+        num2 = float(input("Digite outro número: "))
+
+        soma = num + num2
+        mult = num * num2
+        sub = num - num2
 
         choice = input("você deseja fazer uma soma (1), multiplicação (2) ou subtração (3)? ")
         
         if choice == "1":
-            print(num + num2)
+            resultado = soma
         elif choice == "2":
-            print(num * num2)
+            resultado = mult
         elif choice == "3":
-            print(num - num2)
+            resultado = sub
         else:
             print("escolha inválida.")
+            continue
+
+
+        print("Resultado", resultado)
+
+        raiz = math.sqrt(resultado)
+        print("A raiz é %0.2f" %(raiz))
 main()
-
-
 
