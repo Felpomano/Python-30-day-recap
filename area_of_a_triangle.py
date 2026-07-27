32d5b10bf2542c4d9df2250b246d6e77c062badb
Felpo <70416991+Felpomano@users.noreply.github.com>
2026-07-27 00:49:34 -0300

    Add files via upload
    a for loop that calculate the area of 5 triangles in order.

---
+++ area_of_a_triangle.py
@@ -0,0 +1,6 @@
+for i in range(5):
+    print(f"\nTriangle {i + 1}")
+    base = float(input("Base: "))
+    height = float(input("Height: "))
+    print("Area =", 0.5 * base * height)
+                
