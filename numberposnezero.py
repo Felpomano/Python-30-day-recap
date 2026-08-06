67b3854ea00643834b93ec05e38a438c4f985913
Felpo <70416991+Felpomano@users.noreply.github.com>
2026-08-05 23:30:22 -0300

    Add files via upload
    check if a number is positive, negative, or zero.

---
+++ numberposnezero.py
@@ -0,0 +1,8 @@
+print("Check if a Number is Positive, Negative, or Zero")
+num =  int(input("Input a number: "))
+if num < 0:
+    print(f"the number {num} is negative")
+elif num > 0:
+    print(f"the number {num} is positive")
+else:
+    print(f"the number is zero")
